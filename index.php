027f420ee5d5e91be3a7209ea3ac11abc7b736ca
anandvipul <anandvipul01@gmail.com>
2024-05-11 12:02:26 +0530

    php_date_time

--- index.php
+++ index.php
@@ -6,6 +6,30 @@
     <title>PHP Advanced</title>
 </head>
 <body>
-    
+    <?php
+    echo "Today is ". date("l d.m.Y"); 
+    echo "<br>";
+    echo "ROday is ". date("d.m.YYYY");
+
+    echo "<br>";
+    echo "&copy; 2010 -". date("Y");
+
+    echo "<br>";
+    echo date_default_timezone_get();
+    date_default_timezone_set("Asia/Kolkata");
+    echo "<br>";
+    echo date_default_timezone_get();
+    echo "<br>";
+    echo date("H.i.s.a");
+echo "<br>";
+    $d = mktime(11,14,12,5,1,2001);
+    echo $d;
+    echo "<br>";
+    echo "Created date as ". date("d.m.y. H.i.s a", $d);
+    echo "<br>";
+
+    $d = strtotime("10:30 am april 2023");
+    echo $d;
+    ?>
 </body>
 </html>
