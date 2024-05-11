3824192943d792f25e968f6800aae7c5358985ad
anandvipul <anandvipul01@gmail.com>
2024-05-11 20:42:49 +0530

    php upload

--- index.php
+++ index.php
@@ -41,5 +41,11 @@ fwrite($myFile, $txt);
 fclose($myFile);
 
     ?>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
 </body>
 </html>
