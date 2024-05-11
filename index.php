1b717f249e738dad0626f99db04025e66f99c282
anandvipul <anandvipul01@gmail.com>
2024-05-11 16:45:36 +0530

    file open/read

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
     <title>PHP Advanced</title>
 </head>
 <body>
-    
+    <?php
+echo readfile("temp.txt");
+echo "<hr>";
+echo filesize("temp.txt");
+echo "<hr>";
+$myFile = fopen("temp.txt", "r") or die("Unable to open the file!");
+echo fread($myFile, filesize("temp.txt"));
+fclose($myFile);
+echo "<hr>";
+$myFile = fopen("temp.txt", "r");
+while(!feof($myFile)) {
+    echo fgets($myFile);
+    echo "<br>";
+}
+fclose($myFile);
+    ?>
 </body>
 </html>
