15a3b0df5c5147f4d0f5f6407151e4ec565fd0cb
anandvipul <anandvipul01@gmail.com>
2024-05-12 09:18:41 +0530

    merge

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ setcookie($cookie_name, $cookie_value, time() + 86400*30, "/");
     <title>PHP Advanced</title>
 </head>
 <body>
+<<<<<<< HEAD
 <?php
     if (!isset($_COOKIE[$cookie_name])) {
         echo "Cookie named ". $cookie_name."'is not set!";
@@ -22,5 +23,48 @@ setcookie($cookie_name, $cookie_value, time() + 86400*30, "/");
     }
 
 ?>
+=======
+    <?php
+echo readfile("temp.txt");
+echo "<hr>";
+echo filesize("temp.txt");
+echo "<hr>";
+$myFile = fopen("temp.txt", "r") or die("Unable to open the file!");
+echo fread($myFile, filesize("temp.txt"));
+fclose($myFile);
+echo "<hr>";
+$myFile = fopen("temp.txt", "r");
+while(!feof($myFile)) {
+    echo fgets($myFile);
+    echo "<br>";
+}
+fclose($myFile);
+
+echo "<hr>";
+
+$myFile = fopen("open.txt", "w") or die ("Unable to open the file");
+$txt = "John Doe \n";
+
+fwrite($myFile, $txt);
+
+fwrite($myFile, "My File");
+fclose($myFile);
+
+$myFile = fopen("open.txt", "w") or die ("Unable to open the file");
+$txt = "John Doerr \n";
+
+fwrite($myFile, $txt);
+
+// fwrite($myFile, "My File");
+fclose($myFile);
+
+    ?>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+>>>>>>> php_file_handling
 </body>
 </html>
