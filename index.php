3675e07667a3421cd85b5c4d007a69c85820425d
anandvipul <anandvipul01@gmail.com>
2024-05-12 08:57:27 +0530

    php_setcoocies

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + 86400*30, "/");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +13,14 @@
     <title>PHP Advanced</title>
 </head>
 <body>
-    
+<?php
+    if (!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named ". $cookie_name."'is not set!";
+    } else {
+        echo "Cookie '". $cookie_name. "'is set! <br>";
+        echo "Value is: ".$_COOKIE[$cookie_name];
+    }
+
+?>
 </body>
 </html>
